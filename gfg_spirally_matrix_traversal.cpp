4c07a90129f7199605da72ed653a94830556f7be
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-10-17 23:07:11 +0530

    Spirally traversing a matrix

---
+++ gfg_spirally_matrix_traversal.cpp
@@ -0,0 +1,28 @@
+
+// Q: https://practice.geeksforgeeks.org/problems/spirally-traversing-a-matrix/0
+
+#include<bits/stdc++.h>
+using namespace std;
+int t,n,m,arr[11][11];
+int main()
+{
+    cin >> t;
+    while(t--){
+        cin >> m >> n;
+        for(int i = 0;i < m; ++i) for(int j = 0; j < n; ++j) cin >> arr[i][j];
+        int a = 0,b = n-1,c = m-1, d = 0;
+        while((a <= c)&&(d <= b))
+        {
+        for(int i = d; i <= b; ++i) cout << arr[a][i] << " "; a++;
+        if(a > c) break;
+        for(int i = a; i <= c; ++i) cout << arr[i][b] << " "; b--;
+        if(d > b) break;
+        for(int i = b; i >= d; --i) cout << arr[c][i] << " "; c--;
+        if(a > c) break;
+        for(int i = c; i >= a; --i) cout << arr[i][d] << " "; d++;
+        if(d > b) break;
+        }
+        cout << endl;
+        }
+	return 0;
+}
