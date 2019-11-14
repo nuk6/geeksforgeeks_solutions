7edd9ec8ea8df494fe31381461b8019d0d9ad465
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-11-14 22:04:36 +0530

    Create iheaders.h

---
+++ iheaders.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<queue>
+#include<map>
+#include<stack>
+#include <initializer_list>
+#include<unordered_set>
+#include<unordered_map>
+#include<typeinfo>
+#define pii pair<int,int> 
+#define sz(a) sizeof(a)/sizeof(a[0])
+using namespace std;
+void show(int x){
+    int bit[32];
+    for(int i = 0; i < 32; ++i) {bit[i] = (x&1); x>>=1;}
+    for (size_t i = 0; i < 32; i++)
+    {
+        cout << bit[32-i] << " ";
+    }
+}
