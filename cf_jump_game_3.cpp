5c7162111927406bb0983344392874fc2194d2b9
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2022-05-10 03:12:31 +0530

    Still Incomplete

--- cf_jump_game_3.cpp
+++ cf_jump_game_3.cpp
@@ -1,5 +1,3 @@
-// Problem : https://codeforces.com/problemset/problem/1601/B
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,36 +6,53 @@ int src[10001];
 
 using cvi = const vector<int>&;
 
-void get(cvi a, cvi b) {
+int get(cvi a, cvi b) {
 	queue<int> q;
-	int n= a.size();
+	int n = a.size();
 	q.push(n-1);
 	int mn = INT_MAX;
 	bool found = false;
 	while(!q.empty()) {
 		int idx = q.front(); q.pop();
 		for(int i = 1; i <= a[idx]; ++i) {
-			if(idx-i < mn && idx-i >= 0) {
-				dp[idx-i] = dp[idx] + 1;
-				src[idx-i] = idx;
-				q.push(idx-i+b[idx-i]);
-				mn = idx - i;
-			} else if(idx-i < 0) {
-				//cout << "here" << endl;
-				found = true;
-				cout << dp[idx]+1 << endl;
-				cout << idx << " " << src[idx] << " " << src[src[idx]] << endl;
-				break;
+			int curr = idx-i;
+			if(curr < mn && curr > 0) {
+				dp[curr] = dp[idx]+1;
+				src[curr] = idx;
+				int y = min(curr+b[curr], n-1);
+				dp[y] = dp[curr];
+				q.push(y);
+				mn = curr;
+			} else if(curr < 0) {
+				for (int j = 0; j < n; ++j)
+				{
+					cout << dp[j] << " ";
+				}
+				cout << endl;
+				for (int j = 0; j < n; ++j)
+				{
+					cout << src[j] << " ";
+				}
+				return idx;
 			}
 		}
 	}
-	if(!found)
-		cout << -1 << endl; 
+	return -1; 
 }
 
 int main() {
 	vector<int> a{0, 1, 2, 3, 5, 5, 6, 7, 8, 5};
 	vector<int> b{9, 8, 7, 1, 5, 4, 3, 2, 0, 0};
-	get(a,b);
+	int ans = get(a,b);
+	if(ans != -1) {
+		cout << dp[ans]+1;
+		cout << 0;
+		while(!src[ans]) {
+			cout << src[ans];
+			ans = src[ans];
+		}
+	}
+	else cout << -1;
+	cout << endl <<ans;
 	return 0;
 }
